0f999890e144226dbab94ae71e9cbdb92b827ae4
Gautier Nilhcem <nilhcem@gmail.com>
2012-09-10 22:20:10 +0200

    OrmLite optimizations

---
+++ src/com/nilhcem/xebia/essentials/core/DatabaseConfigUtil.java
@@ -0,0 +1,9 @@
+package com.nilhcem.xebia.essentials.core;
+
+import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
+
+public class DatabaseConfigUtil extends OrmLiteConfigUtil {
+	public static void main(String[] args) throws Exception {
+		writeConfigFile("ormlite_config.txt");
+	}
+}
