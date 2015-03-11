e7ec6003e10cda85173486f03b228949cd4f4b6d
Gautier Nilhcem <nilhcem@gmail.com>
2015-03-11 17:32:57 +0100

    Update tests dependencies

--- scripts/run-tests-spoon.sh
+++ scripts/run-tests-spoon.sh
@@ -3,6 +3,6 @@
 # avoid virtual desktop jumps at mac terminal on test run
 export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
 
-./gradlew clean :essentials-android:spoonDebugTest
+./gradlew clean :essentials-android:spoonDebugAndroidTest
 
 echo "test reports: $(pwd)/essentials-android/build/spoon/debug/index.html"
